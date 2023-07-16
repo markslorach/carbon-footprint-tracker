ee17a60d83d53768231b65bb3a0eba715dd4448b
Mark Slorach <hello@markslorach.com>
2023-07-16 12:12:06 +0100

    updated airports data

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,100 +1,100 @@
-import React, { useState } from 'react'
-import airports from '../data/airports';
-import { postFlight } from '../Services/FlightService';
+import React, { useState } from "react";
+import airports from "../data/airports";
+import { postFlight } from "../Services/FlightService";
 
-
-const SearchForm = ({setOrigin, setDestination, origin, destination, getFlightEmissions, addFlight}) => {
-
+const SearchForm = ({
+  setOrigin,
+  setDestination,
+  origin,
+  destination,
+  getFlightEmissions,
+  addFlight,
+}) => {
   const [formData, setFormData] = useState({
     origin: "",
-    destination: "" 
-  })
+    destination: "",
+  });
 
-// THIS BIT DOESNT WORK FYI
+  // THIS BIT DOESNT WORK FYI
   // const onChange = (event) => {
   //   const newFormData = Object.assign({}, formData);
   //   newFormData[event.target.name] = event.target.value;
   //   setFormData(newFormData)
   //   setOrigin(event.target.value);
-    
+
   // }
 
   const handleDestinationChange = (event) => {
     setDestination(event.target.value);
-  }
+  };
 
   const handleOriginChange = (event) => {
     setOrigin(event.target.value);
-  }
+  };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    getFlightEmissions(origin, destination)
+    getFlightEmissions(origin, destination);
     // postFlight(event).then((data) => {
     //   addFlight(data)
     // })
-  }
+  };
 
+  return (
+    <div>
+      <h3>Search Form</h3>
 
+      <form onSubmit={handleSubmit}>
+        <label>
+          Origin:
+          <input
+            type="text"
+            onChange={handleOriginChange}
+            value={origin}
+            list="origin"
+          />
+          <datalist id="origin">
+            {airports.map((airport) => (
+              <option key={airport.code} value={airport.code}>
+                {airport.name}
+              </option>
+            ))}
+          </datalist>
+        </label>
 
-return (
-  <div>
-  <h3>Search Form</h3>
-  
-  <form onSubmit={handleSubmit}>
-  <label>
-    Origin:
-    <input
-      type="text"
-      onChange={handleOriginChange}
-      value={origin}
-      list="origin"
-    />
-    <datalist id="origin">
-      {airports.map((airport) => (
-        <option key={airport.code} value={airport.code}>
-          {airport.name}
-        </option>
-      ))}
-    </datalist>
-  </label>
+        <label>
+          Departure Date:
+          <input type="date" />
+        </label>
 
-  <label>
-    Origin Date:
-    <input type="date" />
-  </label>
+        <label>
+          Destination:
+          <input
+            type="text"
+            onChange={handleDestinationChange}
+            value={destination}
+            list="destination"
+          />
+          <datalist id="destination">
+            {airports.map((airport) => (
+              <option key={airport.code} value={airport.code}>
+                {airport.name}
+              </option>
+            ))}
+          </datalist>
+        </label>
 
-  <label>
-    Destination:
-    <input
-      type="text"
-      onChange={handleDestinationChange}
-      value={destination}
-      list="destination"
-    />
-    <datalist id="destination">
-      {airports.map((airport) => (
-        <option key={airport.code} value={airport.code}>
-          {airport.name}
-        </option>
-      ))}
-    </datalist>
-  </label>
+        <label>
+          Return Date:
+          <input type="date" />
+        </label>
 
-  <label>
-    Destination Date:
-    <input type="date" />
-  </label>
-
-  <button type="submit" name="submit">
-    Search
-  </button>
-</form>
-
-  </div>
-
-
-)
+        <button type="submit" name="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;

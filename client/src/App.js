cfa30618c19de295dc8c673385d537ca508b4038
Mark Slorach <hello@markslorach.com>
2023-07-16 12:37:43 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
 import MainContainer from './containers/MainContainer';
 
 function App() {
   return (
+    <>
+    <NavBar />
     <MainContainer />
+    </>
   );
 }
 
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const NavBar = () => {
+
+  return (
+      <h1>TravelFootprint.</h1>
+  );
+};
+
+export default NavBar;

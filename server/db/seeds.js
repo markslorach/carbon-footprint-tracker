02798499ad82f55dabe9a66ae1e035794442b7bd
Mark Slorach <hello@markslorach.com>
2023-07-18 13:33:52 +0100

    Branch update

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,18 +7,21 @@ db.flights.insertMany([
         origin: "LAX",
         destination: "JFK",
         footprint: 2200,
-        date: "2023-07-13"
+        date: "2023-07-13",
+        arrivalDate: "2023-07-13"
     },
     {
         origin: "GLA",
         destination: "SIN",
         footprint: 6300,
-        date: "2023-07-13"
+        date: "2023-07-13",
+        arrivalDate: "2023-07-13"
     },
     {
         origin: "ABZ",
         destination: "PEK",
         footprint: 4300,
-        date: "2023-07-13"
+        date: "2023-07-13",
+        arrivalDate: "2023-07-13"
     }
 ]);
